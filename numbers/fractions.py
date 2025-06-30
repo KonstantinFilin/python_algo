53fa18a4ac98db25dba4e09d58d22760b82ae8a2
Konstantin S. Filin <info@kfilin.ru>
2025-06-30 23:13:19 +0300

    Fractions - Basic operations

---
+++ numbers/fractions.py
@@ -0,0 +1,60 @@
+class Fraction:
+    def __init__(self, a, b) -> None:
+        self.a = a
+        self.b = b
+
+    def __str__(self) -> str:
+        return str(self.a) + "/" + str(self.b)
+
+    def add(self, frac2):
+        return Fraction(
+            self.a * frac2.b + frac2.a * self.b,
+            self.b * frac2.b
+        )
+
+    def sub(self, frac2):
+        return Fraction(
+            self.a * frac2.b - frac2.a * self.b,
+            self.b * frac2.b
+        )
+
+    def mult(self, frac2):
+        return Fraction(
+            self.a * frac2.a,
+            self.b * frac2.b
+        )
+
+    def div(self, frac2):
+        return Fraction(
+            self.a * frac2.b,
+            self.b * frac2.a
+        )
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 4:
+        print("Usage: python " + sys.argv[0] + " a/b {+|-|*|/} c/d")
+        exit(1)
+
+    f1a = list(map(int, sys.argv[1].split("/")))
+    f2a = list(map(int, sys.argv[3].split("/")))
+    op = sys.argv[2]
+
+    f1 = Fraction(f1a[0], f1a[1])
+    f2 = Fraction(f2a[0], f2a[1])
+
+    if op not in ['+', '-', '*', '/']:
+        print("Operation must be one of: + - * /")
+        exit(1)
+
+    if op == "+":
+        print(f1.add(f2))
+    elif op == "-":
+        print(f1.sub(f2))
+    elif op == "*":
+        print(f1.mult(f2))
+    elif op == "/":
+        print(f1.div(f2))
+
