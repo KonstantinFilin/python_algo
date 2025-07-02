bdc15e894af608a5471e75f1735a99d87ddac761
Konstantin S. Filin <info@kfilin.ru>
2025-07-02 18:49:35 +0300

    Fractions - Input validation + Building from string

--- numbers/fractions.py
+++ numbers/fractions.py
@@ -11,8 +11,20 @@
 
 class Fraction:
     def __init__(self, a: float, b: float) -> None:
+        if b == 0:
+            raise ValueError("Denominator must not br 0")
+
         self.a = a
         self.b = b
+
+    @staticmethod
+    def build_from_str(s):
+        pos = s.find("/")
+        if pos < 1 or s[-1] == "/":
+            raise ValueError("Fraction must be in format float1/float2")
+
+        f1a = list(map(float, s.split("/")))
+        return Fraction(f1a[0], f1a[1])
 
     def __str__(self) -> str:
         return str(self.a) + "/" + str(self.b)
@@ -67,12 +79,10 @@ if __name__ == "__main__":
         print("Usage: python " + sys.argv[0] + " a/b {+|-|*|/} c/d")
         exit(1)
 
-    f1a = list(map(int, sys.argv[1].split("/")))
-    f2a = list(map(int, sys.argv[3].split("/")))
     op = sys.argv[2]
 
-    f1 = Fraction(f1a[0], f1a[1])
-    f2 = Fraction(f2a[0], f2a[1])
+    f1 = Fraction.build_from_str(sys.argv[1])
+    f2 = Fraction.build_from_str(sys.argv[3])
 
     if op not in ['+', '-', '*', '/']:
         print("Operation must be one of: + - * /")
