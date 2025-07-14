e9137c2b678dd3e17aa3c899e97fdd4b9e6f26c4
Konstantin S. Filin <info@kfilin.ru>
2025-07-14 19:59:06 +0300

    Structure: LinkedList methods

--- structures/linked_list.py
+++ structures/linked_list.py
@@ -20,6 +20,12 @@ class LinkedListItem:
     def get_object(self):
         return self.obj
 
+    def __str__(self) -> str:
+        return str(self.get_object())
+
+    def __repr__(self) -> str:
+        return str(self.get_object())
+
 
 class LinkedList():
     def __init__(self) -> None:
@@ -50,20 +56,168 @@ class LinkedList():
         return self.head is None
 
     def insert_start(self, obj):
-        pass
+        if type(obj).__name__ != "LinkedListItem":
+            raise TypeError("head param must be instance of LinkedListItem class")
+
+        obj.set_next(self.head)
+        self.set_head(obj)
 
     def insert_end(self, obj):
-        pass
+        if type(obj).__name__ != "LinkedListItem":
+            raise TypeError("head param must be instance of LinkedListItem class")
+
+        self.get_last().set_next(obj)
 
     def insert_n(self, obj, n):
-        pass
+        if n < 0:
+            raise IndexError("Element index must be 0 or greater")
 
-    def remove_n(self, obj, n):
-        pass
+        if not self.head:
+            raise IndexError("Empty list")
 
+        c = 0
+        no = self.head
+        no_prev = None
+
+        while no:
+            if c == n:
+                no_prev.set_next(obj)
+                obj.set_next(no)
+                return
+
+            c += 1
+            no_prev = no
+            no = no.get_next()
+
+        raise IndexError(f"Element index must be between 0 and {c - 1}")
+
+    def remove_n(self, n):
+        if n < 0:
+            raise IndexError("Element index must be 0 or greater")
+
+        if not self.head:
+            raise IndexError("Empty list")
+
+        c = 0
+        no = self.head
+        no_prev = None
+
+        while no:
+            if c == n:
+                no_prev.set_next(no.get_next())
+                return
+
+            c += 1
+            no_prev = no
+            no = no.get_next()
+
+        raise IndexError(f"Element index must be between 0 and {c - 1}")
+
+    """
     def change(self, i, j):
-        pass
+        if i < 0 or j < 0:
+            raise IndexError("Element index must be 0 or greater")
 
-    def search_obj(self, obj):
-        pass
+        if not self.head:
+            raise IndexError("Empty list")
 
+        c = 0
+        no = self.head
+        no_prev = None
+        item_i_prev = None
+        item_i = None
+        item_i_next = None
+        item_j_prev = None
+        item_j = None
+        item_j_next = None
+
+        print("A")
+
+        while no:
+            if c == i:
+                item_i_prev = no_prev
+                item_i = no
+                item_i_next = no.get_next()
+
+            if c == j:
+                item_j_prev = no_prev
+                item_j = no
+                item_j_next = no.get_next()
+
+            if item_i and item_j:
+                break
+
+            c += 1
+            no_prev = no
+            no = no.get_next()
+
+        if item_i and item_j:
+            if item_i_prev:
+                item_i_prev.set_next(item_j)
+            if item_i_next:
+                item_j.set_next(item_i_next)
+            if item_j_prev:
+                item_j_prev.set_next(item_i)
+            if item_j_next:
+                item_i.set_next(item_j_next)
+
+        print("B")
+"""
+
+    def search(self, obj):
+        c = 0
+        no = self.head
+
+        while no:
+            if no.get_object() == obj:
+                return c
+
+            c += 1
+            no = no.get_next()
+
+        return -1
+
+    def __str__(self) -> str:
+        items = self.get_items()
+        ret = []
+
+        for i in items:
+            ret.append(str(i.get_object()))
+
+        return ret
+
+    def get_first(self):
+        if not self.head:
+            raise IndexError("Empty list")
+
+        return self.head
+
+    def get_last(self):
+        if not self.head:
+            raise IndexError("Empty list")
+
+        n = self.head
+
+        while n.has_next():
+            n = n.get_next()
+
+        return n
+
+    def get_n(self, n):
+        if n < 0:
+            raise IndexError("Element index must be 0 or greater")
+
+        if not self.head:
+            raise IndexError("Empty list")
+
+        c = 0
+        no = self.head
+
+        while no and c <= n:
+            if c == n:
+                return no
+
+            c += 1
+            no = no.get_next()
+
+        raise IndexError(f"Element index must be between 0 and {c - 1}")
--- structures/test_linked_list.py
+++ structures/test_linked_list.py
@@ -35,15 +35,188 @@ def test_linked_list():
 
     ll.set_head(lli0)
 
-    # assert ll.len() == 4
+    assert ll.len() == 4
     assert not ll.is_empty()
 
     items = ll.get_items()
 
     print(items)
 
-    #assert len(items) == 4
+    assert len(items) == 4
     assert items[0].get_object() == "abc"
     assert items[1].get_object() == "def"
     assert items[2].get_object() == "ghi"
     assert items[3].get_object() == "jkl"
+
+    lli4 = LinkedListItem("ooo")
+    ll.insert_start(lli4)
+    items = ll.get_items()
+
+    print(items)
+
+    assert len(items) == 5
+    assert items[1].get_object() == "abc"
+    assert items[0].get_object() == "ooo"
+
+
+def test_linked_list_insert_end():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi")
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+
+    items = ll.get_items()
+
+    assert len(items) == 3
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "ghi"
+
+    ll.insert_end(lli3)
+    items = ll.get_items()
+
+    assert len(items) == 4
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "ghi"
+    assert items[3].get_object() == "jkl"
+
+    ll.insert_start(LinkedListItem("zzz"))
+    items = ll.get_items()
+
+    assert len(items) == 5
+    assert items[0].get_object() == "zzz"
+    assert items[1].get_object() == "abc"
+    assert items[2].get_object() == "def"
+    assert items[3].get_object() == "ghi"
+    assert items[4].get_object() == "jkl"
+
+
+def test_get_first_get_next():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi", lli3)
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+    first = ll.get_first()
+    last = ll.get_last()
+
+    assert first.get_object() == "abc"
+    assert last.get_object() == "jkl"
+
+
+def test_linked_list_get_n():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi", lli3)
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+
+    n = ll.get_n(0)
+    assert n.get_object() == "abc"
+
+    n = ll.get_n(2)
+    assert n.get_object() == "ghi"
+
+    n = ll.get_n(3)
+    assert n.get_object() == "jkl"
+
+
+def test_linked_list_search():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi", lli3)
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+
+    assert ll.search("abc") == 0
+    assert ll.search("def") == 1
+    assert ll.search("ghi") == 2
+    assert ll.search("jkl") == 3
+
+"""
+
+def test_linked_list_change():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi", lli3)
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+    items = ll.get_items()
+
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "ghi"
+    assert items[3].get_object() == "jkl"
+
+    ll.change(2, 3)
+    items = ll.get_items()
+
+    print("C")
+
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "jkl"
+    assert items[3].get_object() == "ghi"
+
+    print("D")
+"""
+
+
+def test_linked_list_insert_n():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi", lli3)
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+    items = ll.get_items()
+
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "ghi"
+    assert items[3].get_object() == "jkl"
+
+    ll.insert_n(LinkedListItem("ooo"), 2)
+    items = ll.get_items()
+
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "ooo"
+    assert items[3].get_object() == "ghi"
+    assert items[4].get_object() == "jkl"
+
+
+def test_linked_list_remove_n():
+    ll = LinkedList()
+    lli3 = LinkedListItem("jkl")
+    lli2 = LinkedListItem("ghi", lli3)
+    lli1 = LinkedListItem("def", lli2)
+    lli0 = LinkedListItem("abc", lli1)
+
+    ll.set_head(lli0)
+    items = ll.get_items()
+
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "ghi"
+    assert items[3].get_object() == "jkl"
+
+    ll.remove_n(2)
+    items = ll.get_items()
+
+    assert items[0].get_object() == "abc"
+    assert items[1].get_object() == "def"
+    assert items[2].get_object() == "jkl"
